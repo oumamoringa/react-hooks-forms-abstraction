b90dbb58cfbb5633218a9ad7b8aba02b9a08b2c4
oumarichard <richard.ouma@student.moringaschool.com>
2022-09-26 10:59:45 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,6 +24,16 @@ function Form() {
     });
   }
 
+  function handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.type === "chackbox") {
+      value = event.target.checked;
+    }
+    console.log(handleChange())
+
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
